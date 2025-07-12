7ba6e9e7121310f63d7eb701c4700edfbfdd4004
Babjoshlove <Bamideleifeoluwajoshua@gmail.com>
2025-07-12 19:17:00 +0100

    second commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#define the variables
+number1 = 10
+number2 = 5
+#perform the operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+#print the operations
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
