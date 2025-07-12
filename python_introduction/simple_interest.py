67eb987b3a9970fed8d1bf0a1a489e89bae23acb
Babjoshlove <Bamideleifeoluwajoshua@gmail.com>
2025-07-12 19:36:44 +0100

    Third commit for calculating interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# define the 3 variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculate the interest
+simple_interest = principal * rate * time
+#print the interest
+print(f"simple_interest: {simple_interest}")
