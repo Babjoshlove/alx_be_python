9a836ddd6c2d707c351d1135d9fdf6be787e0fa6
Babjoshlove <Bamideleifeoluwajoshua@gmail.com>
2025-07-18 16:19:21 +0100

    initial commit

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+# weather_advice.py
+#prompt the user to select
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+
+# conditions for the weather
+if weather == "sunny":
+  print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+  print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+  print("Make sure to wear a warm coat and a scarf.")
+else:
+  print("Sorry, I don't have recommendations for this weather.")
