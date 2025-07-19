71b0e5715a96ff580db8734160c0ff779cc02dd9
Babjoshlove <Bamideleifeoluwajoshua@gmail.com>
2025-07-19 11:12:02 +0100

    second commit

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,8 @@
 #for loop
 #multiplication_table.py
-number = input("Enter a number to see its multiplication table: ")
-for x in number :
-  for y in number:
-    print(f" {x},{y**y}")
+# Prompt the user to enter a number
+number = int(input("Enter a number to see its multiplication table: "))
+for x in range (1, 11) :
+  result = number * x
+  print(f"{number}*{x} = {result}")
+
