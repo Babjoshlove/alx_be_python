61c2f227cbe21f5526f078f1cd43cb6076e27f8a
Babjoshlove <Bamideleifeoluwajoshua@gmail.com>
2025-07-19 10:35:18 +0100

    Initial commit

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,24 @@
+# daily_reminder.py
+
+print("Welcome to your Daily Task Reminder!")
+
+#Prompt for a Single Task:
+task = input("Enter your task:").lower()
+Priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ")
+#match case
+match Priority :
+  case "high":
+    reminder = (f"High Priority Task: '{task}'")
+  case "low":
+    reminder = (f"Low Priority Task: '{task}'")
+  case "medium":
+    reminder = (f"Medium priority Task: '{task}'")
+  case _:
+    reminder = (f"Invalid priority")
+if time_bound == "yes":
+  reminder += "it requires immediate attention today!"
+else:
+  reminder += "Consider completing it when you have free time."
+  print(reminder)
+
---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,6 @@
+#for loop
+#multiplication_table.py
+number = input("Enter a number to see its multiplication table: ")
+for x in number :
+  for y in number:
+    print(f" {x},{y**y}")
